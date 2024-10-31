ee637d327f1be259ae95fa80ba5f2a40ccb45238
BB-Baron <baron.beyblade@gmail.com>
2024-10-31 14:07:13 +0900

    restore

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // service-worker.js
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -8,4 +9,30 @@ self.addEventListener('activate', function(e) {
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'score.html'
+        'style.css',
+        'style_b.css',
+        'style_g.css',
+        'style_bl.css',
+        'script.js',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+>>>>>>> Stashed changes
